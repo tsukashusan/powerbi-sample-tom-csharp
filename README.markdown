f0cbd34a8c759496d4898d66383065cc7356c501
Shuichi TSUKAMOTO <shuichi.tsukamoto@yahoo.com>
2026-07-22 18:51:17 +0900

    first commit.

---
+++ README.markdown
@@ -0,0 +1,1 @@
+# powerbi-sample-tom-csharp
---
+++ fabric-tom/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+
+// create the connect string
+string workspaceConnection = "powerbi://api.powerbi.com/v1.0/myorg/LearningTOM";
+string connectString = $"DataSource={workspaceConnection};";
+// connect to the Power BI workspace referenced in connect string
+Server server = new Server();
+server.Connect(connectString);
+// enumerate through models in workspace to display their names
+foreach (Database database in server.Databases) {
+  Console.WriteLine(database.Name);
+}
